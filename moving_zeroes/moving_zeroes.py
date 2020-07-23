51470a5fa27d1c2f3e7d356686eef1df34c347da
mtoce <mtoce@villanova.edu>
2020-07-23 13:35:11 -0400

    finished moving zeroes

--- moving_zeroes/moving_zeroes.py
+++ moving_zeroes/moving_zeroes.py
@@ -4,8 +4,25 @@ Returns: a List of integers
 '''
 def moving_zeroes(arr):
     # Your code here
+    count = 0       # count of non-zero elements
 
-    pass
+    # Strategy: grab all non-zero elements and overwrite the beginning
+    # elements up to the count of non-zero elements.
+    # Then, overwrite the elements from the end of the count -> the end of the array
+    # with zeroes
+
+    # traverse array, if elem is non-zero replace elem at index 'count' with elem
+    for i in range(len(arr)):
+        if arr[i] != 0:
+            arr[count] = arr[i]
+            count += 1
+    
+    # once the non-zero elements have overwritten the past elements,
+    # we can overwrite the elements from count -> end of array with zeroes
+    while count < len(arr):
+        arr[count] = 0
+        count += 1
+    return arr
 
 
 if __name__ == '__main__':
